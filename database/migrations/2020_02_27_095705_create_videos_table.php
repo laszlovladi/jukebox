5f78d4737f5c8c3889ce271f32d3dbfb45c2fde9
laszlovladi <laszlo.vladi@gmail.com>
2020-02-27 11:25:25 +0100

    added migrations

---
+++ database/migrations/2020_02_27_095705_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 255);
+            $table->string('code', 127);
+            $table->text('description', 255);
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
